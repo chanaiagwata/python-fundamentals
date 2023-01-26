2365e67c9e71e012ac24e349a529ae6f41b4e189
chanai <chanaiagwata82@gmail.com>
2023-01-27 01:08:19 +0300

    claculate year turns 80

--- module_test.py
+++ module_test.py
@@ -9,13 +9,39 @@
 # y = ceil(x)
 # print(y)
 
-import random
+# import random
 # we can generate a random number using the randit method
 
 # random_number = random.randint(100,6000)
 
 # print(random_number)
-r = random.randint(2, 5)
-p = 3.14
-circumference = 2*p*r
-print(circumference)
+# r = random.randint(2, 5)
+# p = 3.14
+# circumference = 2*p*r
+# print(circumference)
+
+# print("what is your name")
+# name = input()
+
+# print("How old are you")
+# age = int(input())
+
+# print(name)
+# print(age)
+
+# import sys
+# age = sys.argv[2]
+# name = sys.argv[1]
+
+
+# print(name)
+# print(age)
+print("What is your age?")
+age = int(input())
+
+print("What year were your born")
+birthyear = int(input())
+
+print(age)
+print(birthyear)
+print(80+birthyear)
