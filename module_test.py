1d207b3912f4fe321c09994a13e83154cc0c02a8
chanai <chanaiagwata82@gmail.com>
2023-01-26 23:29:59 +0300

    calculate circle circumference using random method

--- module_test.py
+++ module_test.py
@@ -3,8 +3,19 @@
 
 # print(greeting+ ", " + school+ "!")
 
-from math import sqrt, ceil
-x = sqrt(9)
-print(x)
-y = ceil(x)
-print(y)
+# from math import sqrt, ceil
+# x = sqrt(9)
+# print(x)
+# y = ceil(x)
+# print(y)
+
+import random
+# we can generate a random number using the randit method
+
+# random_number = random.randint(100,6000)
+
+# print(random_number)
+r = random.randint(2, 5)
+p = 3.14
+circumference = 2*p*r
+print(circumference)
