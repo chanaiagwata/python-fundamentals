7fcc7b7b4d6ab2e871b886e880c88eb7be2b5deb
chanai <chanaiagwata82@gmail.com>
2023-01-27 17:17:14 +0300

    using the range method to create a list

--- module_test.py
+++ module_test.py
@@ -31,9 +31,32 @@
 
 #Checking for nothing
 # An empty value is automatically considered to be False
-list_a = []
+# list_a = []
 
-if list_a:  #automatically evaluates to false beecause it is empty
-    print("I will not run")
-else:
-    print("I am empty")
+# if list_a:  #automatically evaluates to false beecause it is empty
+#     print("I will not run")
+# else:
+#     print("I am empty")
+
+# Looping
+# Executes code over and over    again
+#for loop
+# used to repeat something repeatedly
+# numbers = [1,2,3,4,5]
+
+# for number in numbers:
+#     print(number)
+#loops through the number list and stores each individual number in a variable called number
+
+# Range()
+# We can use range() in for loops
+# range() returns a list for which we can use to loop through
+#lets use range() to create a list
+# list_a = list(range(0,5))
+# print(list_a)
+
+# the list() method converts it to a list
+for i in range(0,7):
+    print("I would love" + " " +str(i) +" " +"cookies")
+
+# the str() method converts i into a string to allow easy concatenation with the rest of the stringthe str() method converts i into a string to allow easy concatenation with the rest of the string
