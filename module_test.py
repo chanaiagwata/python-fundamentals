b474ec99ea40aefe76aa5929aae493c93ac18075
chanai <chanaiagwata82@gmail.com>
2023-01-29 01:43:08 +0300

    phone feature that counts the number of times a  button is pressed for character to be printed

--- module_test.py
+++ module_test.py
@@ -40,20 +40,52 @@
 
 # setdefault() method
 # When dealing with dictionaries, it is important to set placeholder values to keys before we do any operation to them to prevent unnecessary errors
-print("Enter a string")
-input_string = input()
+# print("Enter a string")
+# input_string = input()
 
-characters = {} #an empty dictionary that stores each individual character and how many times it has been used
-#setdefault
+# characters = {} #an empty dictionary that stores each individual character and how many times it has been used
 
-for character in input_string:
-    characters.setdefault(character, 0)
-    characters[character] = characters[character]+1
+# for character in input_string:
+#     characters.setdefault(character, 0)
+#     characters[character] = characters[character]+1
 
 # used for loop to iterate through character in the input string
 #used setdefault() to create  a key for each character if the character does not exist in the dictionary
 # set default value for each character as 0
 # Lastly we target the key that matches the character and add 1 everytime the character is encountered
-print(characters)
+# print(characters)
 
+print("Enter text")
+
+input_text = input()
+characters = ["a", "b", "c", "e" "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"]
+pressOnce = ["a", "d", "g", "j", "m", "p", "t", "w"]
+pressTwice = ["b", "e", "h", "k", "n", "q", "u", "x"]
+pressThrice = ["c", "f", "i", "l", "o", "r", "v", "y"]
+pressFour = ["s", "z"]
+
+text = {}
+
+
+
+for character in input_text:    
+    if character in pressOnce:
+        text.setdefault(character, 1)
+        continue
+
+    elif character in pressTwice:
+        text.setdefault(character, 2)
+        continue
+
+    elif character in pressThrice:
+        text.setdefault(character, 3)
+        continue
+
+    else:
+        text.setdefault(character, 4)
+
+print(sum(text.values()))
+
+
+    
 
