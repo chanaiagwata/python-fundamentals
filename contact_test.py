06af3a64460c17fbeecc6860b487b6e5ca5f1512
chanai <chanaiagwata82@gmail.com>
2023-01-30 16:09:11 +0300

    creates unittests

---
+++ contact_test.py
@@ -0,0 +1,9 @@
+import unittest #importing the unittest module
+from contact import Contact #importing the contact class
+
+class TestContact(unittest.TestCase):
+    '''
+    Test class that defines test cases for the contact class behaviors
+    Args:
+    unittest.Testcase: TestCase class that helps in creating test cases
+    '''
