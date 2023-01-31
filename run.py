2fc80ea829859a6e81e8e96f4120e049b344eea9
chanai <chanaiagwata82@gmail.com>
2023-02-01 00:28:04 +0300

    adds search contact method

--- run.py
+++ run.py
@@ -74,4 +74,34 @@ def main():
             print ('\n')
         elif short_code == 'dc':
             
-    
+            if display_contacts():
+                print("Here is a list of all your contacts")
+                print('\n')
+
+                for contact in display_contacts():
+                    print(f"{contact.first_name} {contact.last_name}....{contact.phone_number}")
+                    print('\n')
+            else:
+                print('\n')
+                print("You don't seem to have any contacts saved yet")
+        elif short_code == 'fc':
+            print("Enter the number you want to search for")
+
+            search_number = input()
+            if check_existing_contact(search_number):
+                search_contact = find_contact(search_number)
+                print(f"{search_contact.first_name} {search_contact.last_name}")
+
+                print('-' * 20)
+                print(f"Phone number......{search_contact.phone_number}")
+                print(f"Email address.....{search_contact.email}")
+            else:
+                print("That contact does not exist")
+        elif short_code == "ex":
+            print("Bye ......")
+            break
+        else:
+            print("I really didn't get that. Please use the short codes")
+
+if __name__ == '__main__':
+    main()
