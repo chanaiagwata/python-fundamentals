c83ed247481e1ebd6c68ae770346247faf956897
chanai <chanaiagwata82@gmail.com>
2023-01-31 19:07:58 +0300

    adds save contact method to run script

--- run.py
+++ run.py
@@ -38,3 +38,40 @@ def display_contacts():
     Function that returns all the saved contacts
     '''
     return Contact.display_contacts()
+
+def main():
+    print("Hello Welcome to your contact list. What is your name?")
+    user_name = input()
+
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+
+    while True:
+        print("Use short codes : cc - create a new contact, dc - display contacts, fc - find a contact, ex - exit the contact list ")
+
+        short_code = input().lower()
+
+        if short_code == 'cc':
+            print("New Contact")
+            print("-"*10)
+
+            print("First name...")
+            f_name = input()
+
+            print("Last name...")
+            l_name = input()
+
+            print("Phone number...")
+            p_number = input()
+
+            print("Email address...")
+            e_address = input()
+
+            save_contacts(create_contact(f_name,l_name,p_number,e_address)) #create and save new contact
+
+            print('\n')
+            print(f"New Contact {f_name} {l_name} created")
+            print ('\n')
+        elif short_code == 'dc':
+            
+    
